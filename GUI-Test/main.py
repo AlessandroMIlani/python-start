c27d1bd505c7dded4e5e781fc9153373f7ec6f3a
AlessandroMIlani <alessandromilani2000@gmail.com>
2022-08-11 00:08:47 +0200

    Esecuzione Funzione da bottone

--- GUI-Test/main.py
+++ GUI-Test/main.py
@@ -2,10 +2,21 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from more_itertools import sample
+from kivy.properties import ObjectProperty
 
 
 class MainWindow(BoxLayout):
-    pass
+    sub = ObjectProperty(None)
+    
+
+
+    def send_form(self):
+        self.sub.text = "Sended!"
+        form = open("/home/ale/Scrivania/form.txt", "w+")
+        form.write("Nome: " + self.input_name.text + "\n" + 
+                   "Cognome: " + self.input_surname.text + "\n" + 
+                   "Città: " + self.input_city.text + "\n" +  
+                   "Data: " + self.input_age.text + "\n")
 
 
 class MainApp(App):
